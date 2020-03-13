3cb8948fa294ad5da322608f5dee33afcb54fc7c
auxdevelopment <benjamin.au.99@gmx.at>
2020-03-13 16:03:23 +0000

    Move encoding / decoding logic to a seperate static class

---
+++ Fiskaly/SDK/Models/Transformer.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text;
+
+namespace Fiskaly.Client.Models
+{
+    public class Transformer
+    {
+        public static byte[] EncodeHttpBody(byte[] bodyBytes)
+        {
+            return Encoding.UTF8.GetBytes(Convert.ToBase64String(bodyBytes));
+        }
+
+        public static byte[] EncodeJsonRpcRequest(JsonRpcRequest request)
+        {
+            string payload = JsonConvert.SerializeObject(request);
+            return Encoding.UTF8.GetBytes(payload);
+        }
+
+        public static string DecodeBase64Body(byte[] body)
+        {
+            string base64Body = Convert.ToBase64String(body);
+            byte[] decodedBodyBytes = Convert.FromBase64String(base64Body);
+
+            return Encoding.UTF8.GetString(decodedBodyBytes);
+        }
+
+        public static byte[] DecodeBase64BytesToUtf8Bytes(byte[] body)
+        {
+            string base64Body = Convert.ToBase64String(body);
+
+            return Convert.FromBase64String(base64Body);
+        }
+    }
+}
