b804942a7bde7f254e73b99529eaaaebf23ef600
auxdevelopment <benjamin.au.99@gmx.at>
2020-03-30 19:53:40 +0000

    Split JsonRpc models into seperate files for readability

---
+++ Fiskaly/SDK/Models/JsonRpc/Request.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+
+namespace Fiskaly.Client.Models
+{
+    public class JsonRpcParams { }
+
+    public class JsonRpcRequest
+    {
+        [JsonProperty("jsonrpc")]
+        public string JsonRpc { get; set; }
+
+        [JsonProperty("id")]
+        public string RequestId { get; set; }
+
+        [JsonProperty("method")]
+        public string Method { get; set; }
+
+        [JsonProperty("params")]
+        public JsonRpcParams Params { get; set; }
+    }
+
+    public class CreateContextParams : JsonRpcParams
+    {
+        [JsonProperty("api_key")]
+        public string ApiKey { get; set; }
+
+        [JsonProperty("api_secret")]
+        public string ApiSecret { get; set; }
+
+        [JsonProperty("base_url")]
+        public string BaseUrl { get; set; }
+    }
+
+    public class RequestParams : JsonRpcParams
+    {
+        [JsonProperty("method")]
+        public string Method { get; set; }
+
+        [JsonProperty("path")]
+        public string Path { get; set; }
+
+        [JsonProperty("body")]
+        public byte[] Body { get; set; }
+
+        [JsonProperty("query")]
+        public object Query { get; set; }
+
+        [JsonProperty("headers")]
+        public object Headers { get; set; }
+
+        [JsonProperty("context")]
+        public object Context { get; set; }
+    }
+
+    public class ConfigParams : JsonRpcParams
+    {
+        [JsonProperty("debug_level")]
+        public int DebugLevel { get; set; }
+
+        [JsonProperty("debug_file")]
+        public string DebugFile { get; set; }
+
+        [JsonProperty("client_timeout")]
+        public int ClientTimeout { get; set; }
+
+        [JsonProperty("smaers_timeout")]
+        public int SmaersTimeout { get; set; }
+    }
+}
---
+++ Fiskaly/SDK/Models/JsonRpc/Response.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Fiskaly.Client.Models
+{
+    public class JsonRpcResponse<T>
+    {
+        [JsonProperty("jsonrpc")]
+        public string JsonRpc { get; set; }
+
+        [JsonProperty("result")]
+        public T Result { get; set; }
+
+        [JsonProperty("error")]
+        public JsonRpcError Error { get; set; }
+
+        [JsonProperty("id")]
+        public string RequestId { get; set; }
+    }
+
+    public class RequestResult
+    {
+        [JsonProperty("status")]
+        public int Status { get; set; }
+
+        [JsonProperty("headers")]
+        public Dictionary<string, string[]> Headers { get; set; }
+
+        [JsonProperty("body")]
+        public byte[] Body { get; set; }
+
+        [JsonProperty("context")]
+        public string Context { get; set; }
+    }
+
+    public class FiskalyApiError
+    {
+        [JsonProperty("status_code")]
+        public int StatusCode { get; set; }
+
+        [JsonProperty("error")]
+        public string Error { get; set; }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+
+    public class JsonRpcError
+    {
+        [JsonProperty("code")]
+        public int Code { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("data")]
+        public object Data { get; set; }
+    }
+
+    public class ResponseErrorData
+    {
+        [JsonProperty("response")]
+        public RequestResult Response { get; set; }
+    }
+}
