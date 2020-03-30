bc75b7aa6a5ce5db9df9d46e49189dfa2f4be6c6
auxdevelopment <benjamin.au.99@gmx.at>
2020-03-30 19:56:06 +0000

    Add ClientConfiguration class

--- Fiskaly/SDK/Platforms/ClientLibrary.cs
+++ Fiskaly/SDK/Platforms/ClientLibrary.cs
@@ -3,11 +3,31 @@ using System.Runtime.InteropServices;
 
 namespace Fiskaly.Client
 {
+    public enum DebugLevel
+    {
+        NO_OUTPUT = -1,
+        ERRORS_ONLY = 0,
+        ERRORS_AND_WARNINGS = 1,
+        EVERYTHING = 2
+    }
+
+    public class ClientConfiguration
+    {
+        public DebugLevel DebugLevel { get; set; }
+        public string DebugFile { get; set; }
+        public int ClientTimeout { get;set; }
+        public int SmaersTimeout { get; set; }
+    }
+
     public abstract class AbstractClient
     {
         protected static bool Is64BitSystem = IntPtr.Size == 8;
+
         protected const string SYMBOL_INVOKE = "_fiskaly_client_invoke";
         protected const string SYMBOL_FREE = "_fiskaly_client_free";
+
+        protected const string LIB_PREFIX = "com.fiskaly.client";
+        protected const string CLIENT_VERSION = "v1.0.0";
 
         abstract protected IntPtr PerformInvokeForArchitecure(byte[] request);
         abstract protected void PerformFreeForArchitecture(IntPtr allocatedMemory);
