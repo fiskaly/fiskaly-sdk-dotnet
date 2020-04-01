96ff107b46d34f3cdb0809d4677fb93c76b3141d
auxdevelopment <benjamin.au.99@gmx.at>
2020-04-01 21:51:41 +0000

    Add FiskalyHttpClient tests

---
+++ Fiskaly/SDK.Tests/FiskalyHttpClientTests.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fiskaly.Errors;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Fiskaly.Client;
+
+namespace Fiskaly.Tests
+{
+    [TestClass()]
+    public class FiskalyHttpClientTests
+    {
+        // public static readonly string API_KEY = System.Environment.GetEnvironmentVariable("NET_SDK_API_KEY");
+        public static readonly string API_KEY = "key";
+        public static readonly string API_SECRET = "secret";
+        // public static readonly string API_SECRET = System.Environment.GetEnvironmentVariable("NET_SDK_API_SECRET");
+
+        public static readonly string SIGN_API_V1_PATH = "https://kassensichv.io/api/v1";
+        public static readonly string SIGN_API_V0_PATH = "https://kassensichv.io/api/v0";
+
+        public string TSS_ID = Guid.NewGuid().ToString();
+        public string CLIENT_ID = Guid.NewGuid().ToString();
+
+        private FiskalyHttpClient GetClient(string baseUrl)
+        {
+            return new FiskalyHttpClient(API_KEY, API_SECRET, baseUrl);
+        }
+
+        [TestMethod()]
+        public void CanAccessApiV0()
+        {
+            var client = GetClient(SIGN_API_V0_PATH);
+
+            try
+            {
+                var response = client.Request("GET", "/", null, null, null);
+
+                Assert.IsNotNull(response);
+                Assert.AreEqual(200, response.Status);
+            } catch (FiskalyHttpError)
+            {
+                Assert.Fail("Should be able to access v1.");
+            }
+        }
+
+        [TestMethod()]
+        public void CanAccessApiV1()
+        {
+            var client = GetClient(SIGN_API_V1_PATH);
+
+            try
+            {
+                var response = client.Request("GET", "/", null, null, null);
+
+                Assert.IsNotNull(response);
+                Assert.AreEqual(200, response.Status);
+            } catch (FiskalyHttpError)
+            {
+                Assert.Fail("Should be able to access v1.");
+            }
+        }
+        public void CreateClientV1(FiskalyHttpClient client)
+        {
+            var clientSerial = "fiskaly-dotnet-sdk-test-client";
+            var createTssPayload = "{ \"serial_number\": \"" + clientSerial + "\" }";
+            var bodyBytes = Encoding.UTF8.GetBytes(createTssPayload);
+
+            var response = client.Request("PUT", "/tss/" + TSS_ID + "/client/" + CLIENT_ID, bodyBytes, null, null);
+            var decodedBody = Encoding.UTF8.GetString(response.Body);
+            var body = JsonConvert.DeserializeObject<Dictionary<string, object>>(decodedBody);
+
+            Assert.AreEqual(200, response.Status);
+            Assert.AreEqual(clientSerial, body.GetValueOrDefault("serial_number", "NO VALUE"));
+            Assert.AreEqual(CLIENT_ID, body.GetValueOrDefault("_id", CLIENT_ID));
+        }
+
+        public void CreateTssV1(FiskalyHttpClient client)
+        {
+            var descriptionContent = "TSS created by .NET SDK Test CreateClientV1 at " + DateTime.Now.ToString();
+            var createClientPayload = "{ \"description\": \"" + descriptionContent + "\", \"state\": \"UNINITIALIZED\" }";
+
+            var bodyBytes = Encoding.UTF8.GetBytes(createClientPayload);
+
+            var response = client.Request("PUT", "/tss/" + TSS_ID, bodyBytes, null, null);
+
+            var decodedBody = Encoding.UTF8.GetString(response.Body);
+            var body = JsonConvert.DeserializeObject<Dictionary<string, object>>(decodedBody);
+
+            Assert.AreEqual(200, response.Status);
+            Assert.AreEqual(descriptionContent, body.GetValueOrDefault("description", "NO VALUE"));
+            Assert.AreEqual("UNINITIALIZED", body.GetValueOrDefault("state", "NO VALUE"));
+            Assert.AreEqual(TSS_ID, body.GetValueOrDefault("_id", TSS_ID));
+
+            var initializeTssPayload = "{ \"state\": \"INITIALIZED\" }";
+            bodyBytes = Encoding.UTF8.GetBytes(initializeTssPayload);
+
+            var initializeResponse = client.Request("PUT", "/tss/" + TSS_ID, bodyBytes, null, null);
+            Assert.AreEqual(200, initializeResponse.Status);
+        }
+
+        [TestInitialize()]
+        public void InitializeTss()
+        {
+            var client = GetClient(SIGN_API_V1_PATH);
+
+            CreateTssV1(client);
+            CreateClientV1(client);
+        }
+
+        [TestMethod()]
+        public void CanStartTransaction()
+        {
+            var client = GetClient(SIGN_API_V1_PATH);
+
+            var txId = Guid.NewGuid().ToString();
+            var payload = "{ \"state\": \"ACTIVE\", \"client_id\": \"" + CLIENT_ID + "\" }";
+
+            var bodyBytes = Encoding.UTF8.GetBytes(payload);
+
+            var response = client.Request("PUT", "/tss/" + TSS_ID + "/tx/" + txId, bodyBytes, null, null);
+
+            Assert.AreEqual(200, response.Status);
+        }
+
+        [TestMethod()]
+        public void CanConfigureClient()
+        {
+            const int clientTimeout = 12340;
+            const int smaersTimeout = 1212;
+            const DebugLevel debugLevel = DebugLevel.EVERYTHING;
+            const string debugFile = "C:\\temp";
+
+            var client = GetClient(SIGN_API_V1_PATH);
+
+            var response = client.ConfigureClient(new Client.ClientConfiguration
+            {
+                ClientTimeout = clientTimeout,
+                SmaersTimeout = smaersTimeout,
+                DebugLevel = debugLevel
+            });
+
+            Assert.AreEqual(clientTimeout, response.ClientTimeout);
+            Assert.AreEqual(smaersTimeout, response.SmaersTimeout);
+            Assert.AreEqual(debugLevel, response.DebugLevel);
+
+            response = client.ConfigureClient(new Client.ClientConfiguration
+            {
+                DebugFile = "C:\\temp"
+            });
+
+            Assert.AreEqual(clientTimeout, response.ClientTimeout);
+            Assert.AreEqual(smaersTimeout, response.SmaersTimeout);
+            Assert.AreEqual(debugLevel, response.DebugLevel);
+            Assert.AreEqual(debugFile, response.DebugFile);
+        }
+
+        [TestMethod()]
+        public void CanGetVersion()
+        {
+            var client = GetClient(SIGN_API_V1_PATH);
+            var version = client.Version();
+
+            Assert.AreEqual("", version);
+        }
+    }
+}
