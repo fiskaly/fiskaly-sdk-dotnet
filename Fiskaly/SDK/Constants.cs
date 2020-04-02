231c15f15e5f814feb65253a280aeeeffa3972b4
auxdevelopment <benjamin.au.99@gmx.at>
2020-04-02 20:29:05 +0000

    Prepend dotnet to SDK version

--- Fiskaly/SDK/Constants.cs
+++ Fiskaly/SDK/Constants.cs
@@ -2,7 +2,7 @@
 {
     static class Constants
     {
-        public const string SDK_VERSION = "v1.1.0r1";
+        public const string SDK_VERSION = "dotnet-v1.1.0-r1";
         public const string CLIENT_VERSION = "v1.1.0";
     }
 }
