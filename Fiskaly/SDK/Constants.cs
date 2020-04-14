237126d61810b8b506f9c391423f2a29cbb214bf
auxdevelopment <benjamin.au.99@gmx.at>
2020-04-14 10:29:04 +0000

    Bump version to v1.1.200

--- Fiskaly/SDK/Constants.cs
+++ Fiskaly/SDK/Constants.cs
@@ -2,7 +2,7 @@
 {
     static class Constants
     {
-        public const string SDK_VERSION = "dotnet-v1.1.100";
-        public const string CLIENT_VERSION = "v1.1.100";
+        public const string SDK_VERSION = "dotnet-v1.1.200";
+        public const string CLIENT_VERSION = "v1.1.200";
     }
 }
