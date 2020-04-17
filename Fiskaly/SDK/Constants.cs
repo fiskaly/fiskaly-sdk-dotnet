8e21f68c83c9aa535ec953cb39290ccd3249df6a
auxdevelopment <benjamin.au.99@gmx.at>
2020-04-17 17:16:04 +0000

    Fix client version

--- Fiskaly/SDK/Constants.cs
+++ Fiskaly/SDK/Constants.cs
@@ -3,6 +3,6 @@
     static class Constants
     {
         public const string SDK_VERSION = "dotnet-v1.1.301";
-        public const string CLIENT_VERSION = "v1.1.301";
+        public const string CLIENT_VERSION = "v1.1.300";
     }
 }
