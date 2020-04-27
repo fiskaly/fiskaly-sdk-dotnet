a9f2a64fe067f74540abdd05a8efc192e0421725
auxdevelopment <benjamin.au.99@gmx.at>
2020-04-27 19:16:52 +0200

    Add download script

---
+++ download-client.ps1
@@ -0,0 +1,6 @@
+$url = "https://storage.googleapis.com/download/storage/v1/b/fiskaly-cdn/o/clients%2Fcom.fiskaly.client-windows-amd64-v1.1.400.dll?generation=1587652227126443&alt=media"
+$output = bin/Release/netcoreapp.3.1/com.fiskaly.client-windows-amd64-v1.1.400.dll
+$start_time = Get-Date
+echo "Starting Download at $start_time"
+Invoke-WebRequest -Uri $url -OutFile $output
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
