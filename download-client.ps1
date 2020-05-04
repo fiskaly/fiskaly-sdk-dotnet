4154521856554d2f0429cb2e629941efc34e102a
auxdevelopment <benjamin.au.99@gmx.at>
2020-05-04 14:18:26 +0200

    Version bump

--- download-client.ps1
+++ download-client.ps1
@@ -1,9 +1,9 @@
-$url = "https://storage.googleapis.com/download/storage/v1/b/fiskaly-cdn/o/clients%2Fcom.fiskaly.client-windows-amd64-v1.1.400.dll?generation=1587652227126443&alt=media"
+$url = "https://storage.googleapis.com/download/storage/v1/b/fiskaly-cdn/o/clients%2Fcom.fiskaly.client-windows-amd64-v1.1.500.dll?generation=1587652227126443&alt=media"
 
 $outDir = "Fiskaly/SDK.Tests/bin/Release/netcoreapp2.0"
 mkdir -p $outDir
 
-$outFile = "com.fiskaly.client-windows-amd64-v1.1.400.dll"
+$outFile = "com.fiskaly.client-windows-amd64-v1.1.500.dll"
 $outPath = $outDir + "/" + $outFile
 
 $start_time = Get-Date
